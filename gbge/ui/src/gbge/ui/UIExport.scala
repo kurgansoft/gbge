2fe3a011d460dfe4b26e908e2ed92b016c79a19e
kurgan <kurgansoft@protonmail.com>
2021-07-25 12:27:03 +0200

    Default implementations for all the fields in the UIExport trait.

--- gbge/ui/src/gbge/ui/UIExport.scala
+++ gbge/ui/src/gbge/ui/UIExport.scala
@@ -6,15 +6,24 @@ import gbge.ui.eps.player.ClientState
 import gbge.ui.eps.spectator.SpectatorState
 import japgolly.scalajs.react.vdom.TagOf
 import org.scalajs.dom.html.Div
+import japgolly.scalajs.react.vdom.all._
 
 trait UIExport {
-  val playerDisplayer: (ClientState, ClientEventHandler[ClientEvent]) => TagOf[Div]
-  val spectatorDisplayer: (SpectatorState, ClientEventHandler[ClientEvent]) => TagOf[Div]
-  val handleNewFU: (ClientState, FrontendUniverse) => (ClientState, ClientResult)
+  val playerDisplayer: (ClientState, ClientEventHandler[ClientEvent]) => TagOf[Div] = (_,_) => {
+    div(color:="yellow", fontSize:="xx-large", "playerDisplayer is not yet implemented.")
+  }
+  val spectatorDisplayer: (SpectatorState, ClientEventHandler[ClientEvent]) => TagOf[Div] = (_,_) => {
+    div(color:="yellow", fontSize:="xx-large", "spectatorDisplayer is not yet implemented.")
+  }
+  val handleNewFU: (ClientState, FrontendUniverse) => (ClientState, ClientResult) = (clientState, fu) => {
+    (clientState.copy(frontendUniverse = Some(fu)), OK)
+  }
   val handleNewFUForSpectator: (SpectatorState, FrontendUniverse) => (SpectatorState, ClientResult) = (state, fu) => {
     (state.copy(frontendUniverse = Some(fu)), OK)
   }
-  val adminDisplayer: (ClientState, ClientEventHandler[ClientEvent]) => TagOf[Div]
+  val adminDisplayer: (ClientState, ClientEventHandler[ClientEvent]) => TagOf[Div] = (_,_) => {
+    div(color:="yellow", fontSize:="xx-large", "adminDisplayer is not yet implemented.")
+  }
   val metaExtension: (ClientState, ClientEventHandler[ClientEvent]) => TagOf[Div] = (_, _) => {
     import japgolly.scalajs.react.vdom.html_<^._
     <.div()
